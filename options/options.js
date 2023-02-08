2b07db16fc8e622c608fe346a3509f600f0b40f5
Crowdbotics <team@crowdbotics.com>
2023-02-08 08:35:41 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "holy-smoke-38808",
+  url: "https://holy-smoke-38808.botics.co",
+  api: "https://holy-smoke-38808.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
